bbc5f11afb4e2e321a0b92efb9fea57a517b9ce8
Mark Lawlor <mwlawlor@gmail.com>
2024-10-28 14:30:43 +1000

    Fix HrefOutputParams

--- packages/expo-router/src/hooks.ts
+++ packages/expo-router/src/hooks.ts
@@ -3,7 +3,13 @@ import React from 'react';
 import { LocalRouteParamsContext } from './Route';
 import { store, useStoreRootState, useStoreRouteInfo } from './global-state/router-store';
 import { Router } from './imperative-api';
-import { RouteParams, RouteSegments, UnknownOutputParams, InternalRoute } from './types';
+import {
+  RouteParams,
+  RouteSegments,
+  UnknownOutputParams,
+  InternalRoute,
+  RouteOutputParams,
+} from './types';
 
 export function useRootNavigationState() {
   return useStoreRootState();
@@ -156,7 +162,7 @@ export function useGlobalSearchParams() {
 export function useLocalSearchParams<
   TParams extends UnknownOutputParams = UnknownOutputParams,
 >(): TParams;
-export function useLocalSearchParams<TRoute extends InternalRoute>(): RouteParams<TRoute>;
+export function useLocalSearchParams<TRoute extends InternalRoute>(): RouteOutputParams<TRoute>;
 export function useLocalSearchParams() {
   const params = React.useContext(LocalRouteParamsContext) ?? {};
   return Object.fromEntries(
